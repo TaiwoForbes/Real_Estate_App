0d99688a393513ba0dfbe13f980c671ef271b272
TaiwoForbes <soyemihissa2000@gmail.com>
2023-09-09 20:45:34 +0100

    worked on the spinner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
 
 const Header = () => {
+    const [pageState, setpageState] = useState('Sign In')
     const navigate = useNavigate()
+
+    const auth = getAuth()
+    useEffect(()=>{
+        onAuthStateChanged(auth,(user)=>{
+            if(user){
+                setpageState('Profile')
+            }else{
+                setpageState("Sign In")
+            }
+        })
+
+    },[auth])
 
     const location = useLocation()
     const pathMatchRoutes = (route)=>{
@@ -11,7 +25,7 @@ const Header = () => {
         }
     }
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto '>
             <div>
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo " className='h-5 cursor-pointer' onClick={()=>{navigate('/')}}/>
@@ -21,7 +35,7 @@ const Header = () => {
                 <ul className='flex space-x-10 '>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-grey-400 border-b-[3px] border-b-transparent ${pathMatchRoutes('/') && 'text-black border-b-red-500'}`} onClick={()=>{navigate('/')}}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-grey-400 border-b-[3px] border-b-transparent ${pathMatchRoutes('/offers') && 'text-black border-b-red-500'}`} onClick={()=>{navigate('/offers')}}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-grey-400 border-b-[3px] border-b-transparent ${pathMatchRoutes('/sign-in') && 'text-black border-b-red-500'}`} onClick={()=>{navigate('/sign-in')}}>Sign In</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-grey-400 border-b-[3px] border-b-transparent ${(pathMatchRoutes('/sign-in') || pathMatchRoutes('/profile')) && 'text-black border-b-red-500'}`} onClick={()=>{navigate('/profile')}}>{pageState}</li>
                 </ul>
             </div>
         </header>
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet,Navigate } from 'react-router-dom'
 import {useAuthStatus} from '../hooks/useAuthStatus'
+import Spiner from './Spiner'
 
 
 const PrivateRoute = () => {
     const {loggedIn,checkingStatus} = useAuthStatus()
     if(checkingStatus){
         
-        return <h3>Loading.....</h3>
+        return <Spiner/>
         
     }
   return loggedIn ? <Outlet/>:<Navigate to='/sign-in'/>
