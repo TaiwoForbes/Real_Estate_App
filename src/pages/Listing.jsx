af298c2be6164783c3ca224ace356f25bc979ff6
TaiwoForbes <soyemihissa2000@gmail.com>
2023-09-11 01:15:57 +0100

    added share link functionality to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,4 +1,5 @@
 import { doc, getDoc } from "firebase/firestore";
+import {FaShare} from 'react-icons/fa'
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase.config";
@@ -9,6 +10,7 @@ import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper/modules';
 import "swiper/css/bundle";
 
 const Listing = () => {
+const [link,setLink] = useState(false)
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -41,6 +43,20 @@ const Listing = () => {
             )
         })}
     </Swiper>
+
+    <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={()=>{
+        navigator.clipboard.writeText(window.location.href)
+        setLink(true)
+        setTimeout(() => {
+            setLink(false)  
+        }, 1000);
+
+    }}>
+        <FaShare  className="text-lg text-slate-500 "/>
+    </div>
+    {link && (
+      <p className="fixed px-2 top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10">Link Copied</p>  
+    )}
   </main>;
 };
 
