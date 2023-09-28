922dc7f19f0574cc91853622a456c0926abe22ac
TaiwoForbes <soyemihissa2000@gmail.com>
2023-09-28 09:47:59 +0100

    fixed some issues with the rent and sales section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Slider from "../components/Slider";
-import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase.config";
 import { Link } from "react-router-dom";
-import ListingItem from '../components/ListingItem'
+import ListingItem from "../components/ListingItem";
 
 const Home = () => {
   // Offers
@@ -26,7 +33,7 @@ const Home = () => {
             data: doc.data(),
           });
         });
-        setOfferListings(listings)
+        setOfferListings(listings);
         console.log(listings);
       } catch (error) {
         console.log(error);
@@ -43,7 +50,7 @@ const Home = () => {
         const listingsRef = collection(db, "listings");
         const q = query(
           listingsRef,
-          where("type", "==", 'rent'),
+          where("type", "==", "rent"),
           orderBy("timestamp", "desc"),
           limit(4)
         );
@@ -55,7 +62,7 @@ const Home = () => {
             data: doc.data(),
           });
         });
-        setRentListings(listings)
+        setRentListings(listings);
         console.log(listings);
       } catch (error) {
         console.log(error);
@@ -71,7 +78,7 @@ const Home = () => {
         const listingsRef = collection(db, "listings");
         const q = query(
           listingsRef,
-          where("type", "==", 'sale'),
+          where("type", "==", "sale"),
           orderBy("timestamp", "desc"),
           limit(4)
         );
@@ -83,7 +90,7 @@ const Home = () => {
             data: doc.data(),
           });
         });
-        setSaleListings(listings)
+        setSaleListings(listings);
         console.log(listings);
       } catch (error) {
         console.log(error);
@@ -98,13 +105,20 @@ const Home = () => {
         {offerListings && offerListings.length > 0 && (
           <div className="m-2 mb-6">
             <h2 className="px-3 text-2xl mt-6 font-semibold">Recent Offers</h2>
-            <Link to='/offers'>
-              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">Show more offers</p>
-
+            <Link to="/offers">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more offers
+              </p>
             </Link>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {offerListings.map((listing)=>{
-                return <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
+              {offerListings.map((listing) => {
+                return (
+                  <ListingItem
+                    key={listing.id}
+                    listing={listing.data}
+                    id={listing.id}
+                  />
+                );
               })}
             </ul>
           </div>
@@ -112,29 +126,43 @@ const Home = () => {
 
         {rentListings && rentListings.length > 0 && (
           <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold">Places For Rent</h2>
-            <Link to='/category/rent'>
-              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">Show more places for rent</p>
-
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for rent
+            </h2>
+            <Link to="/category/rent">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for rent
+              </p>
             </Link>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {offerListings.map((listing)=>{
-                return <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
-              })}
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+              {rentListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
             </ul>
           </div>
         )}
         {saleListings && saleListings.length > 0 && (
           <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold">Places For Sale</h2>
-            <Link to='/category/sale'>
-              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">Show more places for sale</p>
-
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for sale
+            </h2>
+            <Link to="/category/sale">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for sale
+              </p>
             </Link>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {offerListings.map((listing)=>{
-                return <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
-              })}
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+              {saleListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
             </ul>
           </div>
         )}
